17074979f409bd545b920746a06a0d3d70b38e41
Süreyya Engin <sureyyaengin529@gmail.com>
2025-10-07 00:42:32 +0300

    port changed for vercel

--- backend/server.js
+++ backend/server.js
@@ -57,7 +57,5 @@ app.get("/", (req, res) => {
   res.send("Product API is running 🚀");
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`✅ Server running on http://localhost:${PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
